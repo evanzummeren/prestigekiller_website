48efcb9fb6a5c1e751a79b3a64164b96bca79deb
evanzummeren <erikvanzummeren@gmail.com>
2018-05-01 21:06:12 +0200

    add dia and logo

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,21 @@
 import './modules'
 
-console.log(`app.js has loaded!`)
+/** to do
+		1. timer
+		2. numbers meebewegen
+		3. timer bij iedere numbers resetten
+ **/
+ 
+var currentSlide = 0;
+var numberSlides = document.getElementsByClassName('slide').length;
+
+function showSlide(thisSlide) {
+	for ( var i=0; i<numberSlides; i++){
+		document.getElementsByClassName('slide')[i].style.display = "none";
+	}
+		document.getElementsByClassName('slide')[thisSlide].style.display = "block";	
+}
+
+showSlide(1);
+
+// console.log(document.getElementsByClassName('slide')[0]);
